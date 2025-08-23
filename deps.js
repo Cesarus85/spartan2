767dc1e7574b7c0a9b011754f8796cad265d471c
Cesarus85 <145186748+Cesarus85@users.noreply.github.com>
2025-08-23 22:22:27 +0200

    fix gltfloader module path

--- deps.js
+++ deps.js
@@ -1,3 +1,3 @@
 export * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.179.0/build/three.module.min.js';
 export { VRButton } from 'https://cdn.jsdelivr.net/npm/three@0.179.0/examples/jsm/webxr/VRButton.js';
-export { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.179.0/examples/jsm/loaders/GLTFLoader.js';
+export { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.179.0/examples/jsm/loaders/GLTFLoader.js?module';
